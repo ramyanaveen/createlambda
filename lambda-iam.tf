5ec610c2852c54c19f7d469d5d45b42390c928b2
ramya naveen <ramyananjundappa@gmail.com>
2021-12-29 13:40:37 +0530

    edited k=lambda-iam.tf

--- lambda-iam.tf
+++ lambda-iam.tf
@@ -3,8 +3,11 @@ resource "aws_iam_role_policy" "lambda_ramya_policy" {
   role = "${aws_iam_role.lambda_ramya_role.id}"
   policy = "${file("iam/iam-policy.json")}"
 
+}
+
 
 
   resource "aws_iam_role" "lambda_ramya_role" {
   name = "lambda_ramya_role"
   assume_role_policy = "${file("iam/iam-role.json")}"
+  }
