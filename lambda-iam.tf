b77f3965d47311d196653536f62b52b8508cf5b3
ramya naveen <ramyananjundappa@gmail.com>
2021-12-29 12:57:17 +0530

    edited lambda policy and role names

---
+++ lambda-iam.tf
@@ -0,0 +1,10 @@
+resource "aws_iam_role_policy" "lambda_ramya_policy" {
+  name = "lambda_ramya_policy"
+  role = "${aws_iam_role.lambda_ramya_role.id}"
+  policy = "${file("iam/iam-policy.json")}"
+
+
+
+  resource "aws_iam_role" "lambda_ramya_role" {
+  name = "lambda_ramya_role"
+  assume_role_policy = "${file("iam/iam-role.json")}"
