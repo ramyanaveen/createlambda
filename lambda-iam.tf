33660f4eae9ef9fb7c45fd2b1c798f15063865b4
ramya naveen <ramyananjundappa@gmail.com>
2021-12-29 14:07:42 +0530

    edited k=lambda-iam.tf

--- lambda-iam.tf
+++ lambda-iam.tf
@@ -1,13 +1,35 @@
 resource "aws_iam_role_policy" "lambda_ramya_policy" {
   name = "lambda_ramya_policy"
   role = aws_iam_role.lambda_ramya_role.id
-  policy = "${file("iam/iam-policy.json")}"
-
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
 
 
 
   resource "aws_iam_role" "lambda_ramya_role" {
   name = "lambda_ramya_role"
-  assume_role_policy = "${file("iam/iam-role.json")}"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
   }
